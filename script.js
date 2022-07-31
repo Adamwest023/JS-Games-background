32707f1ef4b9738d6ae330a3f7a548d7ebb0c21b
Adam West <Adamwest023@gmail.com>
2022-07-31 10:01:22 -0500

    created gamespeed variable and set firt bgd layer speed

--- script.js
+++ script.js
@@ -11,18 +11,26 @@ let gameSpeed = 5;
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'assets/background-layers/layer-1.png';
 const backgroundLayer2 = new Image();
-backgroundLayer1.src = 'assets/background-layers/layer-2.png';
+backgroundLayer2.src = 'assets/background-layers/layer-2.png';
 const backgroundLayer3 = new Image();
-backgroundLayer1.src = 'assets/background-layers/layer-3.png';
+backgroundLayer3.src = 'assets/background-layers/layer-3.png';
 const backgroundLayer4 = new Image();
-backgroundLayer1.src = 'assets/background-layers/layer-4.png';
+backgroundLayer4.src = 'assets/background-layers/layer-4.png';
 const backgroundLayer5 = new Image();
-backgroundLayer1.src = 'assets/background-layers/layer-5.png';
+backgroundLayer5.src = 'assets/background-layers/layer-5.png';
 
-
+let x = 0;
+let x2 = 2400;
 function animate() {
-    ctx.drawImage(backgroundLayer3, 0, 0);
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(backgroundLayer4, x, 0);
+    ctx.drawImage(backgroundLayer4, x2, 0);
+    if (x < -2400) x = 2400 + x2 - gameSpeed;
+    else x -= gameSpeed;
+    if (x2 < -2400) x2 = 2400 + x - gameSpeed;
+    else x2 -= gameSpeed;
     requestAnimationFrame(animate);
+
 };
 
 animate();
