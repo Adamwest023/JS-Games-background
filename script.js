efd85a8cf37b74e191a9809994d1a69c23e0f56f
Adam West <Adamwest023@gmail.com>
2022-08-01 22:53:08 -0500

    code refactor

--- script.js
+++ script.js
@@ -19,16 +19,36 @@ backgroundLayer4.src = 'assets/background-layers/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'assets/background-layers/layer-5.png';
 
-let x = 0;
-let x2 = 2400;
+class Layer {
+    constructor(image, speedModifier) {
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400
+        this.height = 700;
+        this.x2 = this.width;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+
+    }
+    update() {
+        this.speed = gameSpeed * this.speedModifier;
+        if (this.x <= -this.speed) {
+            this.x = this.width + this.x - this.speed;
+        }
+        if (this.x2 <= -this.speed) {
+            this.x2 = this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
+    }
+    draw() {
+        
+    }
+}
+
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4, x, 0);
-    ctx.drawImage(backgroundLayer4, x2, 0);
-    if (x < -2400) x = 2400 + x2 - gameSpeed;
-    else x -= gameSpeed;
-    if (x2 < -2400) x2 = 2400 + x - gameSpeed;
-    else x2 -= gameSpeed;
     requestAnimationFrame(animate);
 
 };
