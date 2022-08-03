4194e5fc27519f21bdc5c8aee86b89b4d1e56155
Adam West <Adamwest023@gmail.com>
2022-08-02 21:10:03 -0500

    updated slider function for user control

--- script.js
+++ script.js
@@ -19,11 +19,18 @@ backgroundLayer4.src = 'assets/background-layers/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'assets/background-layers/layer-5.png';
 
+//pulls in our styled slider
 const slider = document.getElementById('slider');
 slider.value = gameSpeed;
-const showGameSpeed = document.getElementById('showGameSpeed'); 
+const showGameSpeed = document.getElementById('showGameSpeed');
 
-showGameSpeed.innerHTML = gameSpeed; 
+//links slider to gameSpeed function
+showGameSpeed.innerHTML = gameSpeed;
+slider.addEventListener('change', function (e) {
+    gameSpeed = e.target.value;
+    showGameSpeed.innerHTML = e.target.value;
+
+})
 
 class Layer {
     constructor(image, speedModifier) {
@@ -58,7 +65,7 @@ class Layer {
 const layer1 = new Layer(backgroundLayer1, 0.2);
 const layer2 = new Layer(backgroundLayer2, 0.4);
 const layer3 = new Layer(backgroundLayer3, 0.6);
-const layer4 = new Layer(backgroundLayer4, 0.8  );
+const layer4 = new Layer(backgroundLayer4, 0.8);
 const layer5 = new Layer(backgroundLayer5, 1);
 
 const gameObjects = [layer1, layer2, layer3, layer4, layer5];
